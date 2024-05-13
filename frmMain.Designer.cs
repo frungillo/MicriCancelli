24dfbd7f74abba1c257955e3e6c12db5944e3d5c
arenamarco <59891668+arenamarco@users.noreply.github.com>
2024-05-13 10:36:09 +0200

    primo

---
+++ frmMain.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace MicriCancelli
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnChiudi = new System.Windows.Forms.Button();
+            this.btnApri = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(281, 389);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Operazioni di lettura";
+            // 
+            // btnChiudi
+            // 
+            this.btnChiudi.BackColor = System.Drawing.Color.White;
+            this.btnChiudi.Image = global::MicriCancelli.Properties.Resources.closeBar1;
+            this.btnChiudi.Location = new System.Drawing.Point(660, 330);
+            this.btnChiudi.Name = "btnChiudi";
+            this.btnChiudi.Size = new System.Drawing.Size(101, 101);
+            this.btnChiudi.TabIndex = 3;
+            this.btnChiudi.UseVisualStyleBackColor = false;
+            this.btnChiudi.Click += new System.EventHandler(this.btnChiudi_Click);
+            // 
+            // btnApri
+            // 
+            this.btnApri.BackColor = System.Drawing.Color.White;
+            this.btnApri.Image = global::MicriCancelli.Properties.Resources.openBar;
+            this.btnApri.Location = new System.Drawing.Point(530, 330);
+            this.btnApri.Name = "btnApri";
+            this.btnApri.Size = new System.Drawing.Size(101, 101);
+            this.btnApri.TabIndex = 2;
+            this.btnApri.UseVisualStyleBackColor = false;
+            this.btnApri.Click += new System.EventHandler(this.btnApri_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnChiudi);
+            this.Controls.Add(this.btnApri);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.Text = "MICRI - Gestione varchi Parcheggio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnApri;
+        private System.Windows.Forms.Button btnChiudi;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace MicriCancelli
+{
+    public partial class frmMain : Form
+    {
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private IntPtr hookID = IntPtr.Zero;
+        private LowLevelKeyboardProc keyboardProc;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            keyboardProc = HookCallback;
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            hookID = SetHook(keyboardProc);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            UnhookWindowsHookEx(hookID);
+        }
+
+        private IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (ProcessModule curModule = Process.GetCurrentProcess().MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+
+                // Converti il codice del tasto in un valore Keys
+                Keys key = (Keys)vkCode;
+                // Controlla se il tasto premuto è il tasto "Invio"
+                if (key == Keys.Return)
+                {
+                    // Aggiungi un ritorno a capo alla TextBox
+                    textBox1.AppendText(Environment.NewLine);
+                }
+                else
+                {
+                    // Altrimenti, aggiungi il carattere alla TextBox
+                    char keyChar = (char)vkCode;
+                    textBox1.AppendText(keyChar.ToString());
+                }
+
+
+
+                // char keyChar = (char)vkCode;
+
+                // Aggiungi il carattere alla TextBox
+               // textBox1.AppendText(DateTime.Now+"- Letto Codice: "+keyChar.ToString()+ "\r\n");
+            }
+            return CallNextHookEx(hookID, nCode, wParam, lParam);
+        }
+
+        // Importazioni delle funzioni di Windows per l'utilizzo dei hook globali
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        // Delegato per la procedura di callback della tastiera
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+
+        private void btnApri_Click(object sender, EventArgs e)
+        {
+                Socket soc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                System.Net.IPAddress ipAdd = System.Net.IPAddress.Parse("10.99.5.101");
+                System.Net.IPEndPoint remoteEP = new IPEndPoint(ipAdd, 80);
+                soc.Connect(remoteEP);
+                byte[] byData = System.Text.Encoding.ASCII.GetBytes("apri*");
+                soc.Send(byData);
+                soc.Close();
+        }
+
+        private void btnChiudi_Click(object sender, EventArgs e)
+        {
+                Socket soc = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                System.Net.IPAddress ipAdd = System.Net.IPAddress.Parse("10.99.5.101");
+                System.Net.IPEndPoint remoteEP = new IPEndPoint(ipAdd, 80);
+                soc.Connect(remoteEP);
+                byte[] byData = System.Text.Encoding.ASCII.GetBytes("chiudi*");
+                soc.Send(byData);
+                soc.Close();
+        }
+
+    }
+}
